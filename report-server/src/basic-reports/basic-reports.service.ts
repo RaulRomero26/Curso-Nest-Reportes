b34f41b148026d939134eaab312dd3e96f4b6e14
Raul Romero <link_2642@hotmail.com>
2025-01-08 18:28:40 -0600

    update: componente de header

--- report-server/src/basic-reports/basic-reports.service.ts
+++ report-server/src/basic-reports/basic-reports.service.ts
@@ -2,7 +2,7 @@ import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 import { TDocumentDefinitions } from 'pdfmake/interfaces';
 import { PrinterService } from 'src/printer/printer.service';
-import { getHelloWorldReport } from 'src/reports';
+import { getEmploymentLetterReport, getHelloWorldReport } from 'src/reports';
 
 @Injectable()
 export class BasicReportsService extends PrismaClient implements OnModuleInit {
@@ -23,6 +23,13 @@ export class BasicReportsService extends PrismaClient implements OnModuleInit {
       const docDefinition = getHelloWorldReport({
         name: 'Raul'
       });
+
+       const doc = this.printerService.createPdf(docDefinition);
+       return doc;
+    }
+
+    async employmentLetter() {
+      const docDefinition = getEmploymentLetterReport();
 
        const doc = this.printerService.createPdf(docDefinition);
        return doc;
---
+++ report-server/src/helpers/date-formatter.ts
@@ -0,0 +1,13 @@
+
+
+export class DateFormater {
+    static formatter = new Intl.DateTimeFormat('es-ES', {
+        year: 'numeric',
+        month: 'long',
+        day: '2-digit'
+    });
+
+    static getDDMMMMYYYY(date: Date): string {
+        return this.formatter.format(date);
+    }
+}
