2bcb92db4ce58c8145391a8d4d0c5c50c22142b1
RaulRomero26 <link_2642@hotmail.com>
2025-01-14 16:56:15 -0600

    layouts personalizados

--- report-server/src/reports/countries.report.ts
+++ report-server/src/reports/countries.report.ts
@@ -52,7 +52,7 @@ export const getCountryReport = ( options : ReportOptions ): TDocumentDefinition
         content: [
            
             {
-                layout: 'lightHorizontalLines', // optional
+                layout: 'customLayout01', // optional
                 table: {
                   // headers are automatically repeated if the table spans over multiple pages
                   // you can declare how many rows should be treated as headers
